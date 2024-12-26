8c6f74271a7df19e526b74f943dd065f4296e102
floatLuciano <lucianoandrade@id.uff.br>
2024-12-26 13:24:05 -0300

    bug fix

--- components/carta/carta.gd
+++ components/carta/carta.gd
@@ -5,6 +5,8 @@ class_name Carta
 
 # cria um sinal (evento) para sinalizar quando a carta morrer
 signal carta_morreu(Carta)
+signal hovered
+signal hovered_off
 
 #@onready var sprite2D: Sprite2D = $Imagem
 
@@ -15,29 +17,30 @@ var titulo: String
 var descricao: String
 
 
+func _ready() -> void:
+	#todas as cartas devem ser filhas de Mão ou vai dar erro
+	get_parent().connect_card_signals(self)
+
 # função "publica" para ser acessada por outros componentes/scripts
 func setSprite(sprite: Texture2D):
 	_sprite = sprite
 
 
-func _on_timer_timeout() -> void:
-	print('Carta morreu')
-	# emite um sinal (evento)
-	carta_morreu.emit(self)
+#func _on_timer_timeout() -> void:
+#	print('Carta morreu')
+#	# emite um sinal (evento)
+#	carta_morreu.emit(self)
 
 func _init(_nivel: int = 0, _titulo: String = "", _descricao: String = ""):
 	nivel = _nivel
 	titulo = _titulo
 	descricao = _descricao
 	
-var is_dragging = false
 
-func _input(event):
-	if event is InputEventMouseButton:
-		if event.pressed and get_global_mouse_position().distance_to(global_position) < 50:  # Verifica clique na carta
-			is_dragging = true
-		elif not event.pressed:
-			is_dragging = false
 
-	if event is InputEventMouseMotion and is_dragging:
-		global_position += event.relative
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered",self)
+	
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off",self)
+	
--- components/mao/mao.gd
+++ components/mao/mao.gd
@@ -7,6 +7,7 @@ class_name Mao
 @export var sprites: Array[Texture2D]
 var card_being_dragged
 var screen_size
+var is_hovering_on_card
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 #func pegarCartaNova() -> Carta:
@@ -18,18 +19,49 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_pos = get_global_mouse_position()
+		# clamp permite definir um minimo e o maximo, fazendo com que a carta nn passe da borda
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x),
 		clamp(mouse_pos.y,0,screen_size.y))
 		
 		
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var carta = raycast_check_for_card()
 		if event.pressed:
-			var carta = raycast_check_for_card()
 			if carta:
 				card_being_dragged = carta
 			else:
 				card_being_dragged = null
+		else:  # Liberação do botão
+			card_being_dragged = null
+				
+func connect_card_signals(carta):
+	carta.connect("hovered", on_hovered_over_card)
+	carta.connect("hovered_off", on_hovered_off_card)
+
+
+func on_hovered_over_card(carta):
+	if !is_hovering_on_card:
+		is_hovering_on_card = true
+		highlight_card(carta, true)
+	
+func on_hovered_off_card(carta):
+	highlight_card(carta, false)
+	var new_card_hovered = raycast_check_for_card()
+	if new_card_hovered:
+		highlight_card(new_card_hovered, true)
+	else:
+		is_hovering_on_card = false
+
+
+func highlight_card(carta, hovered):
+	if hovered:
+		carta.scale = Vector2(1.05, 1.05)
+		carta.z_index = 2
+	else:
+		carta.scale = Vector2(1, 1)
+		carta.z_index = 1
+
 
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
@@ -39,5 +71,24 @@ func raycast_check_for_card():
 	parameters.collision_mask = 1
 	var result = space_state.intersect_point(parameters)
 	if result.size()>0:
-		print(result[0].collider)
+		
+		return get_card_with_highest_z_index(result)
+		
+	print("cheguei aqui")
 	return null
+
+
+# função pra sempre arrastar a carta que tiver no topo
+func get_card_with_highest_z_index(cartas):
+	print (cartas.size())
+	var highest_z_card = cartas[0].collider.get_parent()
+	var highest_z_index = highest_z_card.z_index
+	
+	#loop pelo resto das cartas pra saber qual está mais na frente
+	for i in range (1, cartas.size()):
+		var current_card = cartas[i].collider.get_parent()
+		if current_card.z_index > highest_z_index:
+			highest_z_card = current_card
+			highest_z_index = current_card.z_index
+	return highest_z_card
+		

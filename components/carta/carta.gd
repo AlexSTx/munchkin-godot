000f46b5f2abc5587e1b32e1b354042a3a7b2635
floatLuciano <lucianoandrade@id.uff.br>
2024-12-25 23:22:49 -0300

    movimentacao cartas ok

--- components/carta/carta.gd
+++ components/carta/carta.gd
@@ -6,7 +6,7 @@ class_name Carta
 # cria um sinal (evento) para sinalizar quando a carta morrer
 signal carta_morreu(Carta)
 
-@onready var sprite2D: Sprite2D = $Imagem
+#@onready var sprite2D: Sprite2D = $Imagem
 
 # propriedade "privada" que não deve ser acessada por outros componentes/scripts
 var _sprite: Texture2D
@@ -14,9 +14,6 @@ var nivel: int
 var titulo: String
 var descricao: String
 
-func _ready():
-	# muda o sprite
-	sprite2D.texture = _sprite
 
 # função "publica" para ser acessada por outros componentes/scripts
 func setSprite(sprite: Texture2D):
@@ -32,3 +29,15 @@ func _init(_nivel: int = 0, _titulo: String = "", _descricao: String = ""):
 	nivel = _nivel
 	titulo = _titulo
 	descricao = _descricao
+	
+var is_dragging = false
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.pressed and get_global_mouse_position().distance_to(global_position) < 50:  # Verifica clique na carta
+			is_dragging = true
+		elif not event.pressed:
+			is_dragging = false
+
+	if event is InputEventMouseMotion and is_dragging:
+		global_position += event.relative
--- scenes/Menu.gd
+++ scenes/Menu.gd
@@ -18,4 +18,4 @@ func _on_sair_pressed() -> void:
 	get_tree().quit()
 
 func _on_configuracoes_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file("res://scenes/mesa.tscn")

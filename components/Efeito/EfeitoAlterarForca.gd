4995736c79a27dfd0a9a1b80a22e80723bccc4d0
C. Goncalves <caelgonsalvesrj@hotmail.com>
2024-12-27 18:06:52 -0300

    Refatora Efeito. Cria  EfeitoAlterarForca

---
+++ components/Efeito/EfeitoAlterarForca.gd
@@ -0,0 +1,14 @@
+extends Efeito
+
+class_name EfeitoAlterarForca
+
+@export var valor : int
+
+func _init(p_restricoes : Array[Restricao], p_valor : int) -> void:
+	super(p_restricoes)
+	self.valor = p_valor
+
+func aplicar( alvo : Object ) -> void:
+	super(alvo)
+	var status := alvo.get("status") as StatusEfetivo
+	status.alterar_nivel_efetivo(self.valor)

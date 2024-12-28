ee4f795262a3fd9c7513e4ea29fa4111a85e431a
João Victor Miranda <joaovictormiranda.99@gmail.com>
2024-12-28 16:09:22 -0300

    Added level stage change support

--- components/Fase/Preparo.gd
+++ components/Fase/Preparo.gd
@@ -2,13 +2,15 @@ extends "res://components/Fase/Fase.gd"
 
 class_name Preparo
 
+signal FimDoPreparo
+
 @onready var timer = get_node("Timer")
 @onready var opcoes = get_node("HBoxContainer")
+@onready var confusaopopup = get_node("confusao_popup")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer.start()
-	opcoes.visible = false
+	connect("FimDoPreparo", Callable(Partida.get_turno(), "passar_fase"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,10 +22,13 @@ func TimerTimeout() -> void:
 	opcoes.visible = true
 	timer.stop()
 
+func set_fase() -> void:
+	timer.start()
+	opcoes.visible = false
+	confusaopopup.visible = false
 
 func ChamarMonstro() -> void:
-	var confusao_popup = preload("res://scenes/confusao_popup.tscn").instantiate()
-	add_child(confusao_popup)
+	confusaopopup.visible = true
 	# 1- abre pop up listando as cartas do tipo Monstro que a mão do jogador possui
 	# 2- jogador pode escolher um monstro ( ou mais?? n tenho certeza)
 	# 3- botão de confirmar, que quando apertado, envia um sinal pro Turno passar a fase (sinal ainda a ser implementado)
@@ -31,3 +36,7 @@ func ChamarMonstro() -> void:
 
 func BaterNaPorta() -> void:
 	pass #implementar bater na porta aqui
+
+
+func ConfirmarConfusao() -> void:
+	emit_signal("FimDoPreparo") # emitir sinal pra trocar de fase pra Batalha

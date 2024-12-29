bbbf593ec8c32b2a946917f1840f89825e15622c
Alexsander Soares <alexsandersoarest@gmail.com>
2024-12-29 15:51:07 -0300

    refactor: implemented state design pattern for fases

--- components/Turno.gd
+++ components/Turno.gd
@@ -1,50 +1,45 @@
-extends Node
+class_name Turno extends Node
 
-class_name Turno
+var fase_inicial : Fase
+@onready var fase_atual: Fase = (func () -> Fase:
+	return fase_inicial if fase_inicial != null else get_child(0)
+).call()
 
-var jogador_atual: int #indice do jogador atual na lista de jogadores em Partida
-var fase_atual: int 
-var fases: Array
+var jogador_atual: int # indice do jogador atual na lista de jogadores em Partida
 
-# Called when the node enters the scene tree for the first time.
 
 func _ready() -> void:
-	pass
+	for fase_node: Fase in find_children("*", "Fase"):
+		fase_node.finished.connect(_trocar_de_fase)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	fase_atual.enter("")
 
-func set_turno() -> void:
-	jogador_atual = 0
-	fase_atual = 0
-	fases.append(preload("res://scenes/FasePreparo.tscn").instantiate())
-	fases.append(preload("res://scenes/FaseBatalha.tscn").instantiate())
-	fases.append(preload("res://scenes/FaseFinal.tscn").instantiate())
 
-	add_child(fases[0]) # Adicionar novas fases aqui
-	
-	fases[fase_atual].set_fase()
+func _trocar_de_fase(next_fase_path: String, data: Dictionary) -> void:
+	if not has_node(next_fase_path):
+		printerr(owner.name + ": tentou transicionar para estado " + next_fase_path + ", mas ele não existe.")
+		return 
 
-func passar_fase() -> void:
-	print("Passou de fase")
-	remove_child(fases[fase_atual])
-	#fases[fase_atual].visible = false
-	var lista_jogadores = Partida.get_jogadores()
-	#passa pra próxima fase
-	if fase_atual < fases.size()-1:
-		fase_atual += 1
-	#caso a fase seja a ultima, muda o jogador e volta pra fase inicial (Preparo)
+	var previous_fase_path := fase_atual.name
+	fase_atual.exit()	
+	fase_atual = get_node(next_fase_path)
+	fase_atual.enter(previous_fase_path, data)
+
+
+func _trocar_jogador_atual() -> void:
+	if jogador_atual < len(Partida.get_jogadores())-1:
+		jogador_atual += 1
 	else:
-		fase_atual = 0
-		#if indice_jogador_atual < lista_jogadores.size()-1:
-		#	indice_jogador_atual += 1
-		#else:
-		#	indice_jogador_atual = 0
-	add_child(fases[fase_atual])
-	fases[fase_atual].set_jogador_atual(get_jogador_atual())
-	fases[fase_atual].set_fase()
-	#fases[fase_atual].visible = true
+		jogador_atual = 0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	fase_atual.handle_input(event)
+
+
+func _process(delta: float) -> void:
+	fase_atual.update(delta)
+
 
 func get_jogador_atual() -> Jogador:
 	return Partida.get_jogadores()[jogador_atual]

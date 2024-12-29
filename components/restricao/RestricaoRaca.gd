4d3aa869e6b695f4144e1221f0ff5ece4ce29aae
C. Goncalves <caelgonsalvesrj@hotmail.com>
2024-12-29 17:54:01 -0300

    Ajusta RestricaoRaca e RestricaoClasse para aceitar um efeito inverso

--- components/restricao/RestricaoRaca.gd
+++ components/restricao/RestricaoRaca.gd
@@ -4,12 +4,13 @@ class_name RestricaoRaca
 
 # O nome da classe que satisfaz a restricao
 @export var raca : String
-
-func _init(p_raca : String = "") -> void:
+@export var inverso : bool
+func _init(p_raca : String = "", p_inverso : bool = false) -> void:
 	self.raca = p_raca
+	self.inverso = p_inverso
 
 func satisfaz_condicao( alvo : Jogador ) -> bool:
 	for raca in alvo.equipamentos._racas:
 		if raca.titulo == self.raca:
-			return true
-	return false
+			return true if not inverso else false
+	return false if not inverso else true

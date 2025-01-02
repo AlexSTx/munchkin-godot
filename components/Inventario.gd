7245d256f3345c4d314e8afdede91ff4a53c19d5
Alexsander Soares <alexsandersoarest@gmail.com>
2025-01-02 18:41:36 -0300

    inventory now has working close btn again

--- components/Inventario.gd
+++ components/Inventario.gd
@@ -1,6 +1,4 @@
-extends Node
-
-class_name Inventario
+class_name Inventario extends Node2D
 
 var _classes : Array[Classe]
 var _racas : Array[Raca]
@@ -10,14 +8,51 @@ var _botas : Botas
 var _equipamentoMao : Array[EquipamentoMao]
 var _modificadores : Array
 
-var carta_no_slot = false
-@onready var cena_inventario = preload("res://scenes/inventario.tscn").instantiate()
 
-# Called when the node enters the scene tree for the first time.
+var inventario_bg : Sprite2D 
+
+func _setup_sprite_temp() -> void:
+	var viewport_size = get_viewport_rect().size
+
+	inventario_bg = Sprite2D.new()
+	var texture = GradientTexture2D.new()
+	var gradient = Gradient.new()
+	gradient.colors = [Color.from_hsv(0.42, 0.46, 0.84)]  
+	texture.gradient = gradient
+
+	texture.width = viewport_size.x - 100
+	texture.height = viewport_size.y - 100
+
+	inventario_bg.texture = texture
+	inventario_bg.position = viewport_size / 2
+
+
+func _setup_close_btn() -> void:
+	var btn := Button.new()
+	btn.text = 'X'
+	btn.position = Vector2(inventario_bg.get_rect().size.x - btn.size.x - 50, btn.size.y + 100)
+	btn.z_index = 10
+	add_child(btn)
+	btn.pressed.connect(_on_close_pressed)
+
+
+func _on_close_pressed() -> void:
+	self.visible = false
+
+
 func _ready():
-	pass
+	_setup_sprite_temp()
+	_setup_close_btn()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+
+	add_child(inventario_bg)
+	# var slot = CardSlot.new()
+	# slot.card_entered.connect(func(card): print("Card placed"))
+	# slot.global_position = Vector2(1600, 500)
+	# add_child(slot)
+
+
 func _process(delta: float) -> void:
 	pass
 

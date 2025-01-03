3af842dce05053c9bc02ac8a6123a5c45312374c
João Victor Miranda <joaovictormiranda.99@gmail.com>
2025-01-03 12:59:26 -0300

    Some tweaks that were lacking on this branch

--- components/Fase/FasePreparo.gd
+++ components/Fase/FasePreparo.gd
@@ -1,8 +1,5 @@
 class_name FasePreparo extends Fase
 
-@onready var label_preparo = get_node("Label")
-@onready var label_jogador_vez = get_node("Jogador_atual")
-
 func _ready() -> void:
 	var monstro_slot = Partida.get_mesa().get_monstro_slot()
 	monstro_slot.monstro_added.connect(iniciar_batalha)
@@ -11,23 +8,11 @@ func _process(delta: float) -> void:
 	pass
 
 func enter(previous_fase_path: String, data := {}) -> void:
-	label_preparo.visible = true
-	label_jogador_vez.visible = true
-	
 	Partida.get_mesa().get_porta().setup_click_area()
 	
-	var jogador_atual = Partida.get_turno().get_jogador_atual()
-	if jogador_atual == Partida.get_jogadores()[0]:
-		$"Jogador_atual".text = "Sua vez!"
-	else:
-		$"Jogador_atual".text = "Vez de: " + Partida.get_turno().get_jogador_atual().get_nome()		
-	print("entered PREPARO")
 
 func exit() -> void:
-	label_preparo.visible = false
-	label_jogador_vez.visible = false
 	Partida.get_mesa().get_porta().disable_click_area()
-
 
 func handle_input(_event: InputEvent) -> void:
 	pass

e266c1277baed4f00fe72b00eef386414022fb92
floatLuciano <lucianoandrade@id.uff.br>
2025-01-04 12:52:34 -0300

    batalha v0: fugir funcionando

--- components/Fase/FaseBatalha.gd
+++ components/Fase/FaseBatalha.gd
@@ -1,19 +1,52 @@
 class_name FaseBatalha extends Fase
 
+@onready var mensagem_label: Label = $MensagemLabel
+@onready var botoes_container: GridContainer = $BotoesContainer
+@onready var enfrentar_button: Button = $BotoesContainer/EnfrentarButton
+@onready var fugir_button: Button = $BotoesContainer/FugirButton
+
+
+
 func _ready() -> void:
-	pass
+	# Esconder elementos inicialmente
+	botoes_container.hide()
+	mensagem_label.hide()
 
 func enter(previous_fase_path: String, data := {}) -> void:
+
+	botoes_container.show()
+	mensagem_label.show()
+	mensagem_label.text = "O que você deseja fazer?"
+
+func exit() -> void:
+	botoes_container.hide()
+	mensagem_label.hide()
+	mensagem_label.text = ""
+
+func _on_enfrentar_button_pressed() -> void:
+	# Implementar lógica de combate aqui
 	pass
 
-
-func exit() -> void: 
-	pass
-
-func FimBatalha() -> void:
-	finished.emit("Final", {})
+func _on_fugir_button_pressed() -> void:
+	# Gerar número aleatório entre 1 e 6
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var dado = rng.randi_range(1, 6)
 	
+	# Atualizar mensagem
+	if dado >=5 :
+		mensagem_label.text = "Você rolou %d! Conseguiu fugir!" % dado
+		# Desabilitar botões durante a animação
+		enfrentar_button.disabled = true
+		fugir_button.disabled = true
+		# Esperar 2 segundos e mudar de fase
+		await get_tree().create_timer(2.0).timeout
+		finished.emit("Preparo", {})
+	else:
+		mensagem_label.text = "Você rolou %d! Não conseguiu fugir..." % dado
 
-func set_fase() -> void:
-	print("setou batalha")
+func handle_input(_event: InputEvent) -> void:
+	pass
+
+func update(_delta: float) -> void:
 	pass
--- components/Fase/FasePreparo.gd
+++ components/Fase/FasePreparo.gd
@@ -1,15 +1,17 @@
 class_name FasePreparo extends Fase
 
+@onready var button: Button = $Button
+
 func _ready() -> void:
 	var monstro_slot = Partida.get_mesa().get_monstro_slot()
 	monstro_slot.monstro_added.connect(_iniciar_batalha)
 
 func enter(previous_fase_path: String, data := {}) -> void:
 	Partida.get_mesa().get_porta().setup_click_area()
-	
+	button.show()
 func exit() -> void:
 	Partida.get_mesa().get_porta().disable_click_area()
-
+	button.hide()
 func _iniciar_batalha(monstro: Monstro) -> void:
 	finished.emit("Batalha", {"inimigo": monstro})
 

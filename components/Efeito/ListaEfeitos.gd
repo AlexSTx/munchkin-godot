66f452a9fa6b3684b2db92fc5a49ac00f289cab8
C. Goncalves <caelgonsalvesrj@gmail.com>
2025-01-04 11:27:26 -0300

    pequenas mudanças

--- components/Efeito/ListaEfeitos.gd
+++ components/Efeito/ListaEfeitos.gd
@@ -2,6 +2,8 @@ extends Node
 
 class_name ListaEfeitos
 
+signal lista_alterada
+
 @export var efeitos : Array[Efeito] = []
 
 func _ready() -> void:
@@ -17,3 +19,8 @@ func adicionar_restricao(rest : Restricao) -> void:
 
 func adicionar_efeito(efeito : Efeito) -> void:
 	self.efeitos.push_back(efeito)
+	lista_alterada.emit()
+
+func remover_efeito(index : int):
+	self.efeitos.remove_at(index)
+	lista_alterada.emit()
--- components/Interfaces/StatusEfetivo.gd
+++ components/Interfaces/StatusEfetivo.gd
@@ -8,25 +8,31 @@ class_name StatusEfetivo
 @export var nivel_efetivo : int
 # Podemos usar tanto para determinar a chance de fuga do jogador como um modificador de fuga do monstro
 @export var fuga_efetiva : int
-var efeitos_ativos : Array[Efeito]
+var efeitos_ativos : ListaEfeitos
 
 func _init(
+	p_dono : Carta,
 	p_nivel : int = 1,
-	p_fuga : int = 0,
-	p_efeitos_ativos : Array[Efeito] = []) -> void:
+	p_fuga : int = 0) -> void:
+		self.dono = p_dono
 		self.nivel_efetivo = p_nivel if p_nivel >= 1 else 1
 		self.fuga_efetiva = p_fuga
-		self.efeitos_ativos = p_efeitos_ativos
+		self.efeitos_ativos = ListaEfeitos.new()
 
 func alterar_nivel_efetivo(valor : int):
 	self.nivel_efetivo += valor
 	if self.nivel_efetivo < 1:
 		self.nivel_efetivo = 1
 		
+func recalcular():
+	self.nivel_efetivo = self.dono.nivel
+	self.fuga_efetiva = self.dono.fuga
+	self.efeitos_ativos.aplicar_efeitos(self.dono)
+
 
 func alterar_fuga_efetiva(valor : int):
 	self.fuga_efetiva += valor
 
 func adicionar_efeito_ativo(ef : Efeito):
-	if ef not in self.efeitos_ativos:
-		self.efeitos_ativos.push_back(ef)
+	if ef not in self.efeitos_ativos.efeitos:
+		self.efeitos_ativos.adicionar_efeito(ef)

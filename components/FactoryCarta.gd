d00b39dd71a55ce116293aed42f7bf3a54150987
C. Goncalves <caelgonsalvesrj@hotmail.com>
2025-01-04 14:32:16 -0300

    Adiciona coisa ruim à criação das cartas

--- components/FactoryCarta.gd
+++ components/FactoryCarta.gd
@@ -58,8 +58,8 @@ static func criar_carta(dados : Dictionary) -> Carta:
 			nova_carta.set_script(Monstro)
 			(nova_carta as Monstro).zumbi = dados['SUBTIPO'] == "MORTO VIVO"
 			(nova_carta as Monstro).tesouro = int_if_not_empty(dados['TESOUROS'], 1)
-			if 'COISA_RUIM' in dados:
-				(nova_carta as Monstro).coisa_ruim.efeitos = criar_lista_efeitos(dados['COISA_RUIM'], nova_carta)
+			if 'EFEITOS_COISA_RUIM' in dados:
+				(nova_carta as Monstro).coisa_ruim.efeitos = criar_lista_efeitos(dados['EFEITOS_COISA_RUIM'], nova_carta)
 		"RAÇA":
 			nova_carta.set_script(Raca)
 		_: 
